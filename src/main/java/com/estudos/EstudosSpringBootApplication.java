e3b40719b8befdbfa41baca03ee5965cb1c7ef15
Douglas Nascimento <douglassnascimet@gmail.com>
2023-12-20 16:05:33 -0300

    first commit

---
+++ src/main/java/com/estudos/EstudosSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.estudos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudosSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudosSpringBootApplication.class, args);
+	}
+
+}
