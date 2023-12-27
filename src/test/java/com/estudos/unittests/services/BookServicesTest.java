21856e5a014e97d847e44565f2fbd767e354c880
Douglas Nascimento <douglassnascimet@gmail.com>
2023-12-27 13:19:57 -0300

    Criando a infraestrutura para testes de integração com o TestContainers.

--- src/test/java/com/estudos/unittests/services/BookServicesTest.java
+++ src/test/java/com/estudos/unittests/services/BookServicesTest.java
@@ -17,15 +17,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
---
+++ src/test/java/config/TestConfigs.java
@@ -0,0 +1,5 @@
+package config;
+
+public class TestConfigs {
+    public static final int SERVER_PORT = 8888;
+}
