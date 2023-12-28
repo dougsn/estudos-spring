41f233e5ed8c6e83365b0aed057de7dda714e2d3
Douglas Nascimento <douglassnascimet@gmail.com>
2023-12-28 07:52:44 -0300

    Colocando as migrations para login e user e finalizando mapeamento das entidades.

---
+++ src/main/java/com/estudos/data/model/Permission.java
@@ -0,0 +1,41 @@
+package com.estudos.data.model;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "permission")
+public class Permission implements GrantedAuthority, Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String description;
+
+    public Permission(Long id) {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String getAuthority() {
+        return this.description;
+    }
+}
